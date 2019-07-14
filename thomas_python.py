824b4c7216cbd42fbff532f2437078223b84bdfd
Eldhose Thomas <ejt2018@gmail.com>
2019-07-13 21:14:09 -0500

    test

--- thomas_python.py
+++ thomas_python.py
@@ -3,10 +3,11 @@ import pandas as pd
 import os
 import numpy as np
 # Selectin path & reading the file
-z = r'C:\Users\aatis\Desktop\Applied Analytics\Summer\Project\loan.csv'
+z = r'D:\Final semester\Advanced Applied Analytics\Project\loan_old.csv'
 df = pd.read_csv(os.path.join(os.path.dirname(__file__),z))
 
 #selecting the columns which are less than 10% null values
 df_clean= df.loc[:, df.isnull().mean()<0.1]
 print(list(df_clean.columns.values))
 
+print(df_clean)
