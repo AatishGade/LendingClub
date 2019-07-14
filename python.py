101cde2364df24e029796ef9d8442543255f8128
Aatish Nandkishore Gade <aatish.gade@gmail.com>
2019-07-13 20:07:43 -0500

    reading file

--- python.py
+++ python.py
@@ -2,3 +2,4 @@ import pandas as pd
 import os
 import numpy as np
 z = r'C:\Users\aatis\Desktop\Applied Analytics\Summer\Project\loan.csv'
+df = pd.read_csv(os.path.join(os.path.dirname(__file__),z))
