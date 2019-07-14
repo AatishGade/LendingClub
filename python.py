a0e61571fe7f7c24a24d1ef6e99345e2b21b4eb3
Aatish Nandkishore Gade <aatish.gade@gmail.com>
2019-07-13 20:55:22 -0500

    selected correct range of dataset

--- python.py
+++ python.py
@@ -1,9 +1,11 @@
+# importing libraries 
 import pandas as pd
 import os
 import numpy as np
+# Selectin path & reading the file
 z = r'C:\Users\aatis\Desktop\Applied Analytics\Summer\Project\loan.csv'
 df = pd.read_csv(os.path.join(os.path.dirname(__file__),z))
-print(df)
 
-a=df.isnull().any()
-print(a)
+#selecting the columns which are less than 10% null values
+df_clean= df.loc[:, df.isnull().mean()<0.1]
+print(list(df_clean.columns.values))
