ea7c1d4cde68aeb738f4da3b722b9784dfefe07f
Ryan Allen <rallen21@uco.edu>
2019-07-14 16:13:19 -0500

    Ryan file

--- Ryan.py
+++ Ryan.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 14 12:56:29 2019
+
+@author: tugtrog
+"""
+
 import pandas as pd
 import numpy as np
 import os
@@ -19,6 +26,29 @@ dfdcolumn = dfdrow.loc[:, dfdrow.isnull().mean() < .10]
 #df12 = dfdcolumn.member_id.value_counts().reset_index(name="count").query("count > 1")["index"]
 #Funded by lending club
 dffun = pd.DataFrame(dfdcolumn)
+
+
+dffun["issue_date"] = pd.to_datetime(dffun["issue_d"]).dt.date
+
+dffun['term'] = dffun['term'].str.strip()
+
+
+def f(row):
+    if row['term'] == "36 months":
+        val = 36
+    else: 
+        val = 60
+    return val
+
+dffun["month"] = dffun.apply(f,axis=1)
+
+dffun["future_month"] = dffun.apply(lambda x: x['issue_date'] + pd.offsets.DateOffset(months=x['month']), 1)
+
+#dffun['temp'] = 
+
+dffun["paid_date"] = dffun.loc[df['loan_status']=='Fully Paid','']
+
+hed=dffun.head()
 
 dffun['lc_fun_amt'] = dfdcolumn["funded_amnt"] - dfdcolumn["funded_amnt_inv"]
 
@@ -52,7 +82,7 @@ dfhead = dffun.head(100)
 
 #Rate of Return
 
-dffun["return_per"] = dffun['funded_amnt'] / dffun['total_pymnt_inv']
+dffun["return_per"] = dffun['funded_amnt'] / dffun['total_pymnt']
 
 
 def f(row):
@@ -64,4 +94,5 @@ def f(row):
 
 
 dffun["over_five"] = dffun.apply(f,axis=1)
+
 
