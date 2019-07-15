c66a0b484b07318f0b41f51531df3dcc9cfd737b
Aatish Nandkishore Gade <aatish.gade@gmail.com>
2019-07-15 01:22:19 -0500

    updated working file

--- Aatish.py
+++ Aatish.py
@@ -6,8 +6,8 @@ from string import digits
 z = r'C:\Users\aatis\Desktop\Applied Analytics\Summer\Project\loan.csv'
 x = r'C:\Users\aatis\Desktop\Applied Analytics\Summer\Project\loan.update.csv'
 
-old_data= pd.read_csv(z)
-new_data = pd.read_csv(x)
+old_data= pd.read_csv(z,low_memory= False)
+new_data = pd.read_csv(x, low_memory = False)
 
 # Making seprate Df for grade G
 G = new_data['grade'] == 'G'
@@ -19,6 +19,13 @@ grade_g_88T = old_data[G1]
 p = list(grade_g_88T['loan_status'].values)
 
 # Grade_G with loan status Does not meet the credit policy. Status: Charged off & Fully Paid
+DN_fullpaid = grade_g_88T['loan_status'] == 'Does not meet the credit policy. Status:Fully Paid'
+g_DN_fullpaid = grade_g_88T[DN_fullpaid]
+
+DN_chargedoff = grade_g_88T['loan_status'] == 'Does not meet the credit policy. Status:Charged Off'
+g_DN_chargedoff= grade_g_88T[DN_chargedoff]
+
+
 doNotMeet = grade_g_88T['loan_status'].str.startswith('Does not')
 grade_g_DonotMeet = grade_g_88T[doNotMeet]
 # Making seprate Df for description
@@ -36,6 +43,10 @@ df = pd.concat(frames, sort = False)
 #Removing rows for ID 
 type(df)
 df = df.dropna(axis=0, subset=['id'])
+#Removing rows for grade = G and loan status = "Does not Meet the credit policy. Status: Fully paid & charged off"
+
+df = df[(df['grade'] != "G") & (df['loan_status'] != 'Does not meet the credit policy. Status:Charged Off') & (df['loan_status'] != 'Does not meet the credit policy. Status:Fully Paid')]
+
 
 # Percentage of null values
 Null_percentage = df.isnull().sum()/len(df)*100
@@ -43,79 +54,110 @@ Null_percentage = df.isnull().sum()/len(df)*100
 #selecting the columns which are less than 10% null value
 
 df_clean= df.loc[:, df.isnull().mean()<0.1]
+df_clean = df_clean[(df_clean['loan_status'] != "Issued")]
 col_name = list(df_clean.columns.values)
 
 #Making grade A Late_Fee Average for loan_status = "Fully paid"
 
-A = df_clean['grade'] == 'A'
-grade_A = df_clean[A] 
-FP_A = grade_A['loan_status'] == 'Fully Paid'
-A_fully_Paid = grade_A[FP_A]
-LF_A= A_fully_Paid['total_rec_late_fee'] > 0
-A_FullyPaid_LateFee = A_fully_Paid[LF_A]
-A_Mean = A_FullyPaid_LateFee['total_rec_late_fee'].mean()
+fullyPaid_A = df_clean[(df_clean['grade'] == 'A') & (df_clean['loan_status'] == "Fully Paid")]
+fullyPaid_A_LateFee = fullyPaid_A[(fullyPaid_A['total_rec_late_fee'] > 0)]
+fullPaid_A_Mean = fullyPaid_A_LateFee['total_rec_late_fee'].mean()
 
 
 #Making grade B Late_Fee Average for loan_status = "Fully paid"
 
 
-B = df_clean['grade'] == 'B'
-grade_B = df_clean[B] 
-FP_B = grade_B['loan_status'] == 'Fully Paid'
-B_fully_Paid = grade_B[FP_B]
-LF_B= B_fully_Paid['total_rec_late_fee'] > 0
-B_FullyPaid_LateFee = B_fully_Paid[LF_B]
-B_Mean = B_FullyPaid_LateFee['total_rec_late_fee'].mean()
+fullyPaid_B = df_clean[(df_clean['grade'] == 'B') & (df_clean['loan_status'] == "Fully Paid")]
+fullyPaid_B_LateFee = fullyPaid_B[(fullyPaid_B['total_rec_late_fee'] > 0)]
+fullPaid_B_Mean = fullyPaid_B_LateFee['total_rec_late_fee'].mean()
+
 
 
 
 #Making grade C Late_Fee Average for loan_status = "Fully paid"
 
-C = df_clean['grade'] == 'C'
-grade_C = df_clean[C] 
-FP_C = grade_C['loan_status'] == 'Fully Paid'
-C_fully_Paid = grade_C[FP_C]
-LF_C= C_fully_Paid['total_rec_late_fee'] > 0
-C_FullyPaid_LateFee = C_fully_Paid[LF_C]
-C_Mean = C_FullyPaid_LateFee['total_rec_late_fee'].mean()
+
+fullyPaid_C = df_clean[(df_clean['grade'] == 'C') & (df_clean['loan_status'] == "Fully Paid")]
+fullyPaid_C_LateFee = fullyPaid_C[(fullyPaid_C['total_rec_late_fee'] > 0)]
+fullPaid_C_Mean = fullyPaid_C_LateFee['total_rec_late_fee'].mean()
+
 
 
 #Making grade D Late_Fee Average for loan_status = "Fully paid"
 
 
-D = df_clean['grade'] == 'D'
-grade_D = df_clean[D] 
-FP_D = grade_D['loan_status'] == 'Fully Paid'
-D_fully_Paid = grade_D[FP_D]
-LF_D= D_fully_Paid['total_rec_late_fee'] > 0
-D_FullyPaid_LateFee = D_fully_Paid[LF_D]
-D_Mean = D_FullyPaid_LateFee['total_rec_late_fee'].mean()
+fullyPaid_D = df_clean[(df_clean['grade'] == 'D') & (df_clean['loan_status'] == "Fully Paid")]
+fullyPaid_D_LateFee = fullyPaid_D[(fullyPaid_D['total_rec_late_fee'] > 0)]
+fullPaid_D_Mean = fullyPaid_D_LateFee['total_rec_late_fee'].mean()
 
 #Making grade E Late_Fee Average for loan_status = "Fully paid"
 
-E = df_clean['grade'] == 'E'
-grade_E = df_clean[E] 
-FP_E = grade_E['loan_status'] == 'Fully Paid'
-E_fully_Paid = grade_E[FP_E]
-LF_E= E_fully_Paid['total_rec_late_fee'] > 0
-E_FullyPaid_LateFee = E_fully_Paid[LF_E]
-E_Mean =E_FullyPaid_LateFee['total_rec_late_fee'].mean()
+fullyPaid_E= df_clean[(df_clean['grade'] == 'E') & (df_clean['loan_status'] == "Fully Paid")]
+fullyPaid_E_LateFee = fullyPaid_E[(fullyPaid_E['total_rec_late_fee'] > 0)]
+fullPaid_E_Mean = fullyPaid_E_LateFee['total_rec_late_fee'].mean()
 
 
 #Making grade F Late_Fee Average for loan_status = "Fully paid"
 
-
-F = df_clean['grade'] == 'F'
-grade_F = df_clean[F] 
-FP_F = grade_F['loan_status'] == 'Fully Paid'
-F_fully_Paid = grade_F[FP_F]
-LF_F= F_fully_Paid['total_rec_late_fee'] > 0
-F_FullyPaid_LateFee = F_fully_Paid[LF_F]
-F_Mean=F_FullyPaid_LateFee['total_rec_late_fee'].mean()
-
+fullyPaid_F = df_clean[(df_clean['grade'] == 'F') & (df_clean['loan_status'] == "Fully Paid")]
+fullyPaid_F_LateFee = fullyPaid_F[(fullyPaid_F['total_rec_late_fee'] > 0)]
+fullPaid_F_Mean = fullyPaid_F_LateFee['total_rec_late_fee'].mean()
 # Making a seprate Dict for Grade Late_fee average for loan_status = "Fully paid"
 
-Mean =  {'A': A_Mean, 'B': B_Mean, 'C': C_Mean, 'D': D_Mean , 'E': E_Mean , 'F': F_Mean}
+full_Mean =  {'A': fullPaid_A_Mean, 'B': fullPaid_B_Mean, 'C': fullPaid_C_Mean, 'D': fullPaid_D_Mean , 'E': fullPaid_E_Mean , 'F': fullPaid_F_Mean}
 
 
 zz = df_clean['loan_status']
+
+
+#Making Grade A late_fee average for loan_status active
+Active_A = df_clean[(df_clean['grade'] == 'A') & (df_clean['loan_status'] != "Fully Paid") & (df_clean['loan_status'] !="Charged Off") & (df_clean['loan_status'] != "Default")]
+Active_A_LateFee = Active_A[(Active_A['total_rec_late_fee'] > 0)]
+Active_A_Mean = Active_A_LateFee['total_rec_late_fee'].mean()
+
+#Making Grade B late_fee average for loan_status active
+Active_B = df_clean[(df_clean['grade'] == 'B') & (df_clean['loan_status'] != "Fully Paid") & (df_clean['loan_status'] !="Charged Off") & (df_clean['loan_status'] != "Default")]
+Active_B_LateFee = Active_B[(Active_B['total_rec_late_fee'] > 0)]
+Active_B_Mean = Active_B_LateFee['total_rec_late_fee'].mean()
+
+
+#Making Grade C late_fee average for loan_status active
+Active_C = df_clean[(df_clean['grade'] == 'C') & (df_clean['loan_status'] != "Fully Paid") & (df_clean['loan_status'] !="Charged Off") & (df_clean['loan_status'] != "Default")]
+Active_C_LateFee = Active_C[(Active_C['total_rec_late_fee'] > 0)]
+Active_C_Mean = Active_C_LateFee['total_rec_late_fee'].mean()
+
+#Making Grade D late_fee average for loan_status active
+Active_D = df_clean[(df_clean['grade'] == 'D') & (df_clean['loan_status'] != "Fully Paid") & (df_clean['loan_status'] !="Charged Off") & (df_clean['loan_status'] != "Default")]
+Active_D_LateFee = Active_D[(Active_D['total_rec_late_fee'] > 0)]
+Active_D_Mean = Active_D_LateFee['total_rec_late_fee'].mean()
+
+#Making Grade E late_fee average for loan_status active
+Active_E = df_clean[(df_clean['grade'] == 'E') & (df_clean['loan_status'] != "Fully Paid") & (df_clean['loan_status'] !="Charged Off") & (df_clean['loan_status'] != "Default")]
+Active_E_LateFee = Active_E[(Active_E['total_rec_late_fee'] > 0)]
+Active_E_Mean = Active_E_LateFee['total_rec_late_fee'].mean()
+
+#Making Grade E late_fee average for loan_status active
+Active_F = df_clean[(df_clean['grade'] == 'F') & (df_clean['loan_status'] != "Fully Paid") & (df_clean['loan_status'] !="Charged Off") & (df_clean['loan_status'] != "Default")]
+Active_F_LateFee = Active_F[(Active_F['total_rec_late_fee'] > 0)]
+Active_F_Mean = Active_F_LateFee['total_rec_late_fee'].mean()
+
+#making a new column for Active_Loan_Average_Late_Fee
+df_clean.loc[df.grade =='A', 'Active_Loan_Average_Late_Fee'] = Active_A_Mean
+df_clean.loc[df.grade =='B', 'Active_Loan_Average_Late_Fee'] = Active_B_Mean
+df_clean.loc[df.grade =='C', 'Active_Loan_Average_Late_Fee'] = Active_C_Mean
+df_clean.loc[df.grade =='D', 'Active_Loan_Average_Late_Fee'] = Active_D_Mean
+df_clean.loc[df.grade =='E', 'Active_Loan_Average_Late_Fee'] = Active_E_Mean
+df_clean.loc[df.grade =='F', 'Active_Loan_Average_Late_Fee'] = Active_F_Mean
+
+#Making a new column for Fully_paid_Average_Late_Fee
+df_clean.loc[df.grade =='A', 'Fully_Paid_Loan_Average_Late_Fee'] = fullPaid_A_Mean
+df_clean.loc[df.grade =='B', 'Fully_Paid_Loan_Average_Late_Fee'] = fullPaid_B_Mean
+df_clean.loc[df.grade =='C', 'Fully_Paid_Loan_Average_Late_Fee'] = fullPaid_C_Mean
+df_clean.loc[df.grade =='D', 'Fully_Paid_Loan_Average_Late_Fee'] = fullPaid_D_Mean
+df_clean.loc[df.grade =='E', 'Fully_Paid_Loan_Average_Late_Fee'] = fullPaid_E_Mean
+df_clean.loc[df.grade =='F', 'Fully_Paid_Loan_Average_Late_Fee'] = fullPaid_F_Mean
+
+
+
+
+rt = df_clean.head(100)
